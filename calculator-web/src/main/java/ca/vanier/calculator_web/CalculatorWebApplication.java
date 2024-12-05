845f6bd35fc5c8191d6b8bc08adec10730f0d4c1
Leonardo Mascarenhas <leonardo.pmasc@gmail.com>
2024-12-05 18:03:45 -0500

    Add calculator-web

---
+++ calculator-web/src/main/java/ca/vanier/calculator_web/CalculatorWebApplication.java
@@ -0,0 +1,13 @@
+package ca.vanier.calculator_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWebApplication.class, args);
+	}
+
+}
---
+++ calculator-web/src/test/java/ca/vanier/calculator_web/CalculatorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.vanier.calculator_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
