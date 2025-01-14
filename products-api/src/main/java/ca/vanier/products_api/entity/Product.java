d5597d845cae614d22e45bba108e4b5b300d7922
Kevin Britten <kevinjbritten@gmail.com>
2025-01-14 18:06:31 -0500

    replace category with type Category in Product.java

--- products-api/src/main/java/ca/vanier/products_api/entity/Product.java
+++ products-api/src/main/java/ca/vanier/products_api/entity/Product.java
@@ -6,19 +6,21 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Product {
-    
+
     @Id
     @GeneratedValue
     private Long id;
     private String descr;
     private double price;
 
-    // Assignment: Replace it by a @Entity
-    private String category;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Category category;
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<Tag> tags;
@@ -26,30 +28,39 @@ public class Product {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getDescr() {
         return descr;
     }
+
     public void setDescr(String descr) {
         this.descr = descr;
     }
+
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
-    public String getCategory() {
+
+    public Category getCategory() {
         return category;
     }
-    public void setCategory(String category) {
+
+    public void setCategory(Category category) {
         this.category = category;
     }
+
     public List<Tag> getTags() {
         return tags;
     }
+
     public void setTags(List<Tag> tags) {
         this.tags = tags;
     }
