f39d61c1066a0a68ab51629ca32d3302dfff1890
Leonardo Mascarenhas <leonardo.pmasc@gmail.com>
2025-01-14 17:12:27 -0500

    Implement Tag entity & relate to Product

--- products-api/src/main/java/ca/vanier/products_api/entity/Product.java
+++ products-api/src/main/java/ca/vanier/products_api/entity/Product.java
@@ -1,8 +1,12 @@
 package ca.vanier.products_api.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,5 +21,39 @@ public class Product {
     private String descr;
     private double price;
     private String category;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Tag> tags;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescr() {
+        return descr;
+    }
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
 
 }
