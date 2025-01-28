5c9ed14d812823b24b3f005a02e76f183315c15b
Leonardo Mascarenhas <leonardo.pmasc@gmail.com>
2025-01-28 17:38:14 -0500

    Implement conditionals to handle locale problems & add comments

--- products-api/src/main/java/ca/vanier/products_api/controller/GreetingController.java
+++ products-api/src/main/java/ca/vanier/products_api/controller/GreetingController.java
@@ -23,20 +23,47 @@ public class GreetingController {
         @RequestHeader(name = "Accept-Language", required = false) Locale locale,
         @RequestParam String name) {
 
-        // https://docs.spring.io/spring-framework/reference/web/webmvc/mvc-servlet/localeresolver.html#mvc-localeresolver-session
+        // If the locale is not informed by the client, use the default locale
+        if (locale == null) {
+            locale = Locale.getDefault();
+        }
+
+        // If the locale is informed by the client, but the region is not, use the default region
+        if (!locale.getLanguage().isEmpty() && locale.getCountry().isEmpty()) {
+            if (locale.getLanguage().equals("en")) {
+                locale = new Locale("en", "CA");
+            } else if (locale.getLanguage().equals("fr")) {
+                locale = new Locale("fr", "CA");
+            }
+        }
+        
+        // SessionLocaleResolver
+        // Doc: https://docs.spring.io/spring-framework/reference/web/webmvc/mvc-servlet/localeresolver.html#mvc-localeresolver-session
         // SessionLocaleResolver resolver = new SessionLocaleResolver();
         // resolver.setDefaultLocale(locale);
 
+        // Locale context based on the user's request
+        // Create Currency Formatter using the given local (received by the client)
         NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale);
+        // Format the balance using the Currency Formatter
         String balance = currencyFormat.format(12345.67);
         System.out.println("Balance: " + balance);
     
-        String greetingMessage = messageSource.getMessage("customerBalance", new Object[]{name, balance}, locale);
+        // Compile the message using the message source
+        String greetingMessage = messageSource.getMessage(
+            // Message key
+            "customerBalance", 
+            // Arguments to be replaced in the message
+            new Object[]{name, balance},
+            // Locale containing the language and region informed by the client
+            locale);
+        
         return ResponseEntity.ok(greetingMessage);
     }
 
     @GetMapping("/greeting")
-    public ResponseEntity<String> getMethodName(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+    public ResponseEntity<String> getMethodName(
+        @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
         return ResponseEntity.ok(messageSource.getMessage("greeting", null, locale));
     }
     
