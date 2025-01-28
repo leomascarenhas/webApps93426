6ebcd6363558e426c57be9e536e2cba8cba644fd
Leonardo Mascarenhas <leonardo.pmasc@gmail.com>
2025-01-28 06:05:56 -0500

    Implement Customer Balance

--- products-api/src/main/java/ca/vanier/products_api/controller/GreetingController.java
+++ products-api/src/main/java/ca/vanier/products_api/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package ca.vanier.products_api.controller;
 
+import java.text.NumberFormat;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,6 +17,23 @@ public class GreetingController {
 
     @Autowired
     private MessageSource messageSource;
+
+    @GetMapping("/balance")
+    public ResponseEntity<String> getAccountTotal(
+        @RequestHeader(name = "Accept-Language", required = false) Locale locale,
+        @RequestParam String name) {
+
+        // https://docs.spring.io/spring-framework/reference/web/webmvc/mvc-servlet/localeresolver.html#mvc-localeresolver-session
+        // SessionLocaleResolver resolver = new SessionLocaleResolver();
+        // resolver.setDefaultLocale(locale);
+
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale);
+        String balance = currencyFormat.format(12345.67);
+        System.out.println("Balance: " + balance);
+    
+        String greetingMessage = messageSource.getMessage("customerBalance", new Object[]{name, balance}, locale);
+        return ResponseEntity.ok(greetingMessage);
+    }
 
     @GetMapping("/greeting")
     public ResponseEntity<String> getMethodName(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
